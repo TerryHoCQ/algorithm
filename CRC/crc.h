0d55d65a2050e9f18e452776eb04e77c00ee5a2f
ash256 <1034512163@qq.com>
2022-05-08 16:05:26 +0800

    add crc code

--- CRC/crc.h
+++ CRC/crc.h
@@ -2,8 +2,17 @@
 #define __CRC_H__
 #include <stdio.h>
 
+typedef unsigned char ubit_t;
+
 int CRC8(unsigned char* data, int size);
 int CRC16(unsigned char* data, int size);
 int CRC32(unsigned char* data, int size);
+
+//±ÈÌØCRC 
+int crc_fix(ubit_t *bits, int len, int poly, int width, int init, int xorout);
+#define crc8_poly7(x, y) crc_fix(x, y, 0x7, 8, 0, 0)
+#define crc10_poly24f(x, y) crc_fix(x, y, 0x24F, 10, 0, 0)
+
+void crc_table(int poly, int width);
 
 #endif
